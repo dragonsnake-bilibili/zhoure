ed4887a7669c32f463146b0ad39a42780eed88d4
dragonsnake-bilibili <169534749+dragonsnake-bilibili@users.noreply.github.com>
2024-10-07 18:41:01 +0800

    init

--- main.js
+++ main.js
@@ -1,69 +1,54 @@
 window.addEventListener("load", async () => {
   // select elements
-  const canvas_container = document.getElementById("canvas-container");
-  const background_canvas = document.getElementById("background");
-  const selection_canvas = document.getElementById("selection");
-  const control_container = document.getElementById("control-container");
-  const bounding_box_controls = document.getElementById("set-bounding-box");
-  const frequent_controls = document.getElementById("frequent-controls");
-  const watermark_checkbox = document.getElementById("add-watermark");
-  const preset_selector = document.getElementById("preset-selector");
+  const canvas = document.getElementById("canvas");
+  const processor = document.createElement("canvas");
 
   // create contexts
-  const background_context = background_canvas.getContext("2d");
-  const selection_context = selection_canvas.getContext("2d");
+  const context = canvas.getContext("2d");
+  const processor_context = processor.getContext("2d");
 
   // listeners
-  async function selector_change() {
-    const preset = JSON.parse(preset_selector.value);
-    const response = await fetch(preset.image);
+  async function load_background() {
+    const response = await fetch("zhoure.jpg");
     const image = await response.blob();
     const bitmap = await window.createImageBitmap(image);
-    canvas_container.style.height = `${bitmap.height}px`;
-    canvas_container.style.width = `${bitmap.width}px`;
-    background_canvas.height = bitmap.height;
-    background_canvas.width = bitmap.width;
-    selection_canvas.height = bitmap.height;
-    selection_canvas.width = bitmap.width;
-    background_context.drawImage(bitmap, 0, 0);
-    selection_context.strokeRect(...preset["x-y"], ...preset["width-height"]);
+    canvas.height = bitmap.height;
+    canvas.width = bitmap.width;
+    processor.height = bitmap.height;
+    processor.width = bitmap.width;
+    context.drawImage(bitmap, 0, 0);
+    context.strokeRect(320, 271, 462, 260);
+    const text = "拖拽或点击选择图片";
+    context.font = "52px 'lxgw-wenkai-lite'";
+    context.fillText(text, 319, 391);
+    processor_context.drawImage(bitmap, 0, 0);
   }
 
-  async function setup_listeners() {
-    preset_selector.addEventListener("change", selector_change);
+  await load_background();
+  function drag_helper(event) {
+    event.stopPropagation();
+    event.preventDefault();
   }
-
-  // helpers
-  function render_watermark(canvas, canvas_context) {
-    canvas_context.font = "bold 24px serif";
-    const text = `访问${window.location.href}制作类似图片`;
-    const measurement = canvas_context.measureText(text);
-    // const width = measurement.actualBoundingBoxRight + measurement.actualBoundingBoxLeft;
-    canvas_context.fillText(text, measurement.actualBoundingBoxLeft, measurement.actualBoundingBoxAscent);
+  canvas.addEventListener("dragenter", drag_helper);
+  canvas.addEventListener("dragover", drag_helper);
+  async function produce_result(file) {
+    const bitmap = await window.createImageBitmap(file);
+    processor_context.drawImage(bitmap, 320, 271, 462, 260);
+    const downloader = document.createElement("a");
+    downloader.href = processor.toDataURL("image/png");
+    downloader.download = `${crypto.randomUUID()}.png`;
+    downloader.click();
   }
-
-  async function load_presets() {
-    // load presets from preset file and add which as selections
-    const result = await fetch("data/presets.json");
-    const presets = await result.json();
-    presets.forEach((element, index) => {
-      const selection = document.createElement("option");
-      selection.value = JSON.stringify(element);
-      selection.innerText = element.name;
-      if (index === 0) {
-        selection.selected = true;
-      }
-      preset_selector.appendChild(selection);
+  canvas.addEventListener("drop", async (event) => {
+    drag_helper(event);
+    await produce_result(event.dataTransfer.files[0]);
+  });
+  canvas.addEventListener("click", async () => {
+    const selector = document.createElement("input");
+    selector.type = "file";
+    selector.addEventListener("change", async (event) => {
+      await produce_result(event.target.files[0]);
     });
-    await selector_change();
-  }
-
-  // setup font
-  background_canvas.font = "80px";
-
-  await setup_listeners();
-  await load_presets();
-  if (watermark_checkbox.checked) {
-    render_watermark(background_canvas, background_context);
-  }
+    selector.click();
+  });
 });
